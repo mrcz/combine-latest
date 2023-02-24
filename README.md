7e5cbb3752f33dc4ee2f5ecba55ac91fb3f6f0a4
Marcus Svensson <marcus@lincopia.com>
2023-02-24 18:57:46 +0100

    Add map_latest and map_latest_opt for streams that aren't Clone
    Standardize on _opt suffix for functions that return options.

---
+++ README.md
@@ -0,0 +1,61 @@
+# combine-latest
+
+Combines two streams into a new stream which yields tuples with the latest values from each input
+stream. Inspired by Rx's combineLatest.
+
+```rust
+use async_stream::stream;
+use combine_latest::combine_latest;
+
+fn combine_weather_data_streams(
+    temperature: impl Stream<i32>,
+    weather_notes: impl Stream<String>,
+) -> Stream<String> {
+    stream! {
+        for await (t, n) in combine_latest(temperature, weather_notes) {
+            yield format!("Temperature {t}°, note: {n}");
+        }
+    }
+}
+```
+
+`combine_latest` won't yield its first tuple until both input streams have yielded a value. If you
+need to get items as soon as the first is available, there is a `combine_latest_optional` function
+that yields `(Option<T1>, Option<T2>)` tuples.
+
+As values come in over time on the `temperature` and `weather_notes` streams, `combine_latest` and
+`combine_latest_optional` will yield values like so:
+
+|time| temperature  | weather_notes  | combine_latest        | combine_latest_optional           |
+|----|--------------|----------------|-----------------------|-----------------------------------|
+| 0  | 25           |                |                       | (Some(25), None)                  |
+| 1  | 26           |                |                       | (Some(26), None)                  |
+| 2  |              | Low visibility | (26, "Low visibility")| (Some(26), Some("Low visibility"))|
+| 3  |              | Foggy          | (26, "Foggy")         | (Some(26), Some("Foggy"))         |
+| 4  | 25           |                | (25, "Foggy")         | (Some(25), Some("Foggy"))         |
+
+
+Since the same input value might be returned several times in the output stream, the items that the
+input streams yield *must implement Clone.*
+
+For types that don't implement Clone, it's possible to use the function `map_latest`:
+
+```rust
+fn combine_weather_data_streams(
+    temperature: impl Stream<NonCloneTemperature>,
+    weather_notes: impl Stream<String>,
+) -> Stream<String> {
+    stream! {
+        for await output in combine_latest(
+            temperature,
+            weather_notes,
+            |t, n| format!("Temperature {t}°, note: {n}"),
+        ) {
+            yield output;
+        }
+    }
+}
+
+```
+
+To add to you project: `cargo add combine-latest`
---
+++ src/stream_combiners.rs
@@ -0,0 +1,87 @@
+use std::future;
+
+use either::Either;
+use futures_core::Stream;
+use futures_util::{stream::select, StreamExt};
+
+/// Combines two streams into a new stream that always contains the latest items from both streams
+/// as a tuple. This stream won't yield a tuple until both input streams have yielded at least one
+/// item each.
+pub fn combine_latest<T1: Clone, T2: Clone>(
+    s1: impl Stream<Item = T1>,
+    s2: impl Stream<Item = T2>,
+) -> impl Stream<Item = (T1, T2)> {
+    combine_latest_opt(s1, s2).filter_map(|(v1, v2)| future::ready(v1.zip(v2)))
+}
+
+#[deprecated(since = "1.1.0", note = "Use combine_latest_opt instead")]
+pub fn combine_latest_optional<T1: Clone, T2: Clone>(
+    s1: impl Stream<Item = T1>,
+    s2: impl Stream<Item = T2>,
+) -> impl Stream<Item = (Option<T1>, Option<T2>)> {
+    combine_latest_opt(s1, s2)
+}
+
+/// Combines two streams into a new stream, yielding tuples of `(Option<T1>, Option<T2>)`. The
+/// stream starts yielding tuples as soon as one of the input streams yields an item, and the one
+/// that has not yet yielded has a corresponding `None` in its field of the tuple.
+pub fn combine_latest_opt<T1: Clone, T2: Clone>(
+    s1: impl Stream<Item = T1>,
+    s2: impl Stream<Item = T2>,
+) -> impl Stream<Item = (Option<T1>, Option<T2>)> {
+    let mut current1 = None;
+    let mut current2 = None;
+    select(s1.map(Either::Left), s2.map(Either::Right)).map(move |tuple| {
+        match tuple {
+            Either::Left(i1) => current1 = Some(i1),
+            Either::Right(i2) => current2 = Some(i2),
+        };
+        (current1.clone(), current2.clone())
+    })
+}
+
+/// Combines two streams into a new stream and apply the given function to each item. The function
+/// takes references as arguments, so unlike `combine_latest` the types T1 and T2 don't have to
+/// implement `Clone`. The returned stream won't yield until both streams have yielded at least one
+/// item each.
+pub fn map_latest<T1, T2, U>(
+    s1: impl Stream<Item = T1>,
+    s2: impl Stream<Item = T2>,
+    mut f: impl for<'a, 'b> FnMut(&'a T1, &'b T2) -> U,
+) -> impl Stream<Item = U> {
+    let mut current1 = None;
+    let mut current2 = None;
+
+    select(s1.map(Either::Left), s2.map(Either::Right)).filter_map(move |tuple| {
+        match tuple {
+            Either::Left(i1) => current1 = Some(i1),
+            Either::Right(i2) => current2 = Some(i2),
+        };
+        future::ready(
+            current1
+                .as_ref()
+                .zip(current2.as_ref())
+                .map(|args| f(&args.0, &args.1)),
+        )
+    })
+}
+
+/// Combines two streams into a new stream and apply the given function to each item. The function
+/// takes references as arguments, so unlike `combine_latest` the types T1 and T2 don't have to
+/// implement `Clone`. The returned stream will yield as soon as one input stream yields.
+pub fn map_latest_opt<T1, T2, U>(
+    s1: impl Stream<Item = T1>,
+    s2: impl Stream<Item = T2>,
+    mut f: impl for<'a, 'b> FnMut(&'a Option<T1>, &'b Option<T2>) -> U,
+) -> impl Stream<Item = U> {
+    let mut current1 = None;
+    let mut current2 = None;
+
+    select(s1.map(Either::Left), s2.map(Either::Right)).map(move |tuple| {
+        match tuple {
+            Either::Left(i1) => current1 = Some(i1),
+            Either::Right(i2) => current2 = Some(i2),
+        };
+        f(&current1, &current2)
+    })
+}
